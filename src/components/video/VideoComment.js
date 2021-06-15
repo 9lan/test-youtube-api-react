6003c8a1477412f9c9b687ae5087b778d46df3fc
Andang Vijayanto Utomo <adyayutti.wijang.saraswati-2018@fkh.unair.ac.id>
2021-06-15 15:14:33 +0700

    fix performance and styling issues

--- src/components/video/VideoComment.js
+++ src/components/video/VideoComment.js
@@ -5,19 +5,25 @@ export class VideoComment extends React.Component {
   render() {
     return (
       <div className="ui comments">
-        <h3 className="ui dividing header">Comments</h3>
+        <h3 className="ui dividing header">{"Comments"}</h3>
         <div className="comment">
-          <Link className="avatar">
-            <img src="" alt="avatar" />
+          <Link>
+            <span className="avatar">
+              <img src="" alt="avatar" />
+            </span>
           </Link>
           <div className="content">
-            <Link className="author">Matt</Link>
+            <Link>
+              <span className="author">{"Matt"}</span>
+            </Link>
             <div className="metadata">
-              <span className="date">Today at 5:42PM</span>
+              <span className="date">{"Today at 5:42PM"}</span>
             </div>
             <div className="text">How artistic!</div>
             <div className="actions">
-              <Link className="reply">Reply</Link>
+              <Link>
+                <span className="reply">{"Reply"}</span>
+              </Link>
             </div>
           </div>
         </div>
