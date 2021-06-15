74fd22f6ee886547939a5a5ed1840053c84dd525
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-06-15 09:53:56 +0700

    first commit

---
+++ src/components/Routes.js
@@ -0,0 +1,13 @@
+// import {Switch, Route} from 'react-router-dom'
+
+// const Routes = () => {
+//     return (
+//         <>
+//             <Switch>
+//                 <Route exact path="/" component={} />
+//             </Switch>
+//         </>
+//     )
+// }
+
+// export default Routes;
---
+++ src/components/VideoItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class VideoItem extends React.Component {
+  render() {
+    return <div>Video Item</div>;
+  }
+}
